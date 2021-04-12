58ed0439cccd636f91fc704d4e71cff3c3f9c01e
Eternal36 <33805342+Eternal36@users.noreply.github.com>
2021-04-12 19:07:24 +0300

    test

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by Дмитрий Бельков on 11.04.2021.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var description: String
+    var isFavourite: Bool
+}
